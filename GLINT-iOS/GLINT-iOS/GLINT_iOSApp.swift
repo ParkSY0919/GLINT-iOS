556515edaab8f80c8391bf6640f081abb511f351
ParkSY0919 <qhr498@gmail.com>
2025-05-09 23:39:39 +0900

    [Setting/#1] init GLINT Project

---
+++ GLINT-iOS/GLINT-iOS/GLINT_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLINT_iOSApp.swift
+//  GLINT-iOS
+//
+//  Created by 박신영 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GLINT_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
