a7021994d07c5ce7b0e54d1627df1d6bb9925831
ParkSY0919 <qhr498@gmail.com>
2025-05-28 19:58:53 +0900

    [Fix/#9] User Repository, UserCase 수정

--- GLINT-iOS/GLINT-iOS/Source/Domain/Repository/UserRepository.swift
+++ GLINT-iOS/GLINT-iOS/Source/Domain/Repository/UserRepository.swift
@@ -10,7 +10,7 @@ import Foundation
 struct UserRepository {
     var checkEmailValidation: (_ request: CheckEmailValidationRequest) async throws -> Void
     var signUp: (_ request: SignUpRequest) async throws -> SignUpResponse
-    var signIn:(_ request: SignInRequest) async throws -> SignInResponse
-    var signInApple:(_ request: SignInRequestForApple) async throws -> SignInResponse
-    var signInKakao:(_ request: SignInRequestForKakao) async throws -> SignInResponse
+    var signIn: (_ request: SignInRequest) async throws -> SignInResponse
+    var signInApple: (_ request: SignInRequestForApple) async throws -> SignInResponse
+    var signInKakao: (_ request: SignInRequestForKakao) async throws -> SignInResponse
 }
--- GLINT-iOS/GLINT-iOS/Source/Domain/UseCase/UserUseCase.swift
+++ GLINT-iOS/GLINT-iOS/Source/Domain/UseCase/UserUseCase.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct UserUseCase {
-    var checkEmailValidation: (_ request: CheckEmailValidationRequest) async throws -> Void
-    var signUp: (_ request: SignUpRequest) async throws -> SignUpResponse
-    var signIn:(_ request: SignInRequest) async throws -> SignInResponse
-    var signInApple:(_ request: SignInRequestForApple) async throws -> SignInResponse
-    var signInKakao:(_ request: SignInRequestForKakao) async throws -> SignInResponse
+    var checkEmailValidation: (_ request: CheckEmailValidationRequest) async throws -> Bool
+    var signUp: (_ request: SignUpRequest) async throws -> SignInEntity
+    var signIn: (_ request: SignInRequest) async throws -> SignInEntity
+    var signInApple: (_ request: SignInRequestForApple) async throws -> SignInEntity
+    var signInKakao: (_ request: SignInRequestForKakao) async throws -> SignInEntity
 }
