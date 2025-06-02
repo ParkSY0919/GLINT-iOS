89fae95334143aafd1bd1dc12b8a616ac7614f2c
ParkSY0919 <qhr498@gmail.com>
2025-06-02 20:17:50 +0900

    [Feat/#13] Mapper 구현

---
+++ GLINT-iOS/GLINT-iOS/Source/Data/Mapper/PhotoMetadataMapper.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoMetadataMapper.swift
+//  GLINT-iOS
+//
+//  Created by 박신영 on 6/2/25.
+//
+
+import Foundation
+
+struct PhotoMetadataMapper {
+    static func map(from response: PhotoMetadataResponse) -> PhotoMetadataModel {
+        return .init(
+            camera: response.camera,
+            metaData:
+                [FilterValueFormatter.photoMetaDataFormat(
+                    lensInfo: response.lensInfo,
+                    focalLength: response.focalLength,
+                    aperture: response.aperture,
+                    iso: response.iso),
+                 MegapixelCalculator.calculateMPString(
+                    width: response.pixelWidth,
+                    height: response.pixelHeight,
+                    fileSize: response.fileSize),
+                ],
+            latitude: response.latitude ?? 0.0,
+            longitude: response.longitude ?? 0.0
+        )
+    }
+}
